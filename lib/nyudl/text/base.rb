43ddf26eb9c3cc42dbad05298ec03690eaf7870d
Joseph Pawletko <jgpawletko@gmail.com>
2013-10-02 15:41:42 -0400

    snapshot

--- lib/nyudl/text/base.rb
+++ lib/nyudl/text/base.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 module Nyudl
   module Text
     # Public: Class to validate and rename NYU DLTS Text objects.
@@ -104,6 +106,9 @@ module Nyudl
       def analyzed?
         @analyzed
       end
+      def recognized?
+        analyzed? ? @errors[:unrecognized].empty? : nil
+      end
 
       private
 
@@ -113,7 +118,9 @@ module Nyudl
       # Returns nothing.
       # Raises  nothing.
       def analyze_text
-        @errors = Hash.new { |_h, k| _h[k] = [] }
+        @errors  = Hash.new { |_h, k| _h[k] = [] }
+        @renames = {}
+        @names   = {}
 
         Dir.chdir(@dir) do
           if Dir.glob('*').length == 0
@@ -139,7 +146,7 @@ module Nyudl
 
       def execute_rename_plan
         raise "Cannot rename.  Text not analyzed." unless analyzed?
-        raise "Cannot rename.  Errors detected."   unless @errors.empty?
+        raise "Cannot rename.  Errors detected."   unless @errors.values.inject(true) {|memo, value| memo && value.empty?}
         @renames.keys.sort.each do |k|
           FileUtils.mv(File.join(@dir, @renames[k].fname),
                        File.join(@dir, @renames[k].newname),
@@ -149,152 +156,3 @@ module Nyudl
     end
   end
 end
-
-
-
-# -------------------------------------------------------------------------------
-=begin
-require_relative '../text/filename'
-require 'fileutils'
-require 'pp'
-
-
-
-
-module CCGUtil
-  # Look and Feel Text
-  class Laft < Thor
-    class_option :verbose, :type => :boolean
-    class_option :noop,    :type => :boolean
-
-    option :dir,        :required => true,  :aliases => :d
-    option :prefix,     :required => true,  :aliases => :p
-    option :new_prefix, :required => false, :aliases => :n
-    option :force,      :required => false, :aliases => :f, :type => :boolean, :default => false
-
-    option :fr_digits_max, :required => false, :type => :numeric, :default => 2 # max allowable digits in fr fragments
-    option :fr_digits_out, :required => false, :type => :numeric, :default => 2 # # of digits in fr output fragments
-    option :bk_digits_max, :required => false, :type => :numeric, :default => 2 # max allowable digits in bk fragments
-    option :bk_digits_out, :required => false, :type => :numeric, :default => 2 # # of digits in bk output fragments
-    option :in_digits_max, :required => false, :type => :numeric, :default => 2 # max allowable digits in insert fragments
-    option :in_digits_out, :required => false, :type => :numeric, :default => 2 # # of digits in insert output fragments
-
-    desc "fix_names", "bulk filename correction from known patterns to canonical pattern"
-    def fix_names
-
-      # extract parameters
-      dir    = options[:dir]
-      prefix = options[:prefix]
-      errors = []
-
-      # assert directory
-      raise "ERROR: #{dir} is not a directory" unless File.directory?(dir)
-
-      puts "-------------------------------------------------------------------------------"
-      puts "INCOMING FILENAME ANALYSIS"
-      puts "-------------------------------------------------------------------------------"
-
-      # hash to hold Filename objects keyed by new name
-      renames = {}
-      all_newnames = {}
-      found_errors = false
-
-      # exit if no files found in target directory
-      Dir.chdir(dir) do
-        if Dir.glob('*').length == 0
-          $stderr.puts "ERROR: No files found in #{dir}."
-          puts "OPERATION ABORTED"
-          puts "-------------------------------------------------------------------------------"
-          exit 1
-        end
-      end
-
-      Dir.foreach(dir) do |f|
-        next if (f == '.' || f == '..')
-
-        raise "error: found directory. cannot process." if File.directory?(f)
-
-        begin
-#         i = Filename.new(f, prefix, :new_prefix => options[:new_prefix])
-          i = Text::Filename.new(f, prefix, options)
-          errors << "unrecognized filename: #{File.join(dir,i.fname)}" unless i.recognized?
-        end
-
-        # only add to hash if rename is required
-        renames[i.newname] = i if i.rename?
-        printf("%30s   ->   %30s\n", i.fname, i.newname) if i.rename?
-        # add to all_names hash for sequence check later
-        #all_names[i.newname] = i
-      end
-
-      # run sequence check
-      # approach:
-      # keep separate hash of all filenames
-      # sort by new name key
-      # sequence through names
-      # cases:
-      #   one m, one d
-      #   multiple m files for one d files (this is an oversize)
-      #   target
-      #   TODO: think about having an exceptions array...
-
-
-      unless errors.empty?
-        puts "ERRORS DETECTED.\n#{errors.join("\n")}\n\nPLEASE CORRECT ERRORS AND RUN AGAIN...\n"
-        puts "OPERATION ABORTED"
-        puts "-------------------------------------------------------------------------------"
-        exit 1
-      end
-      puts "OK! All current filename patterns recognized."
-
-      puts "-------------------------------------------------------------------------------"
-      puts "RENAME PLAN"
-      puts "-------------------------------------------------------------------------------"
-
-      renames.keys.sort.each do |k|
-        printf("%30s   ->   %30s\n", renames[k].fname, renames[k].newname)
-      end
-
-      if renames.empty?
-        puts "no rename operations required"
-        puts "thank you for using #$0 "
-        puts "goodbye..."
-        puts "-------------------------------------------------------------------------------"
-        return true
-      end
-
-      puts "-------------------------------------------------------------------------------"
-      $stdout.flush
-      $stderr.flush
-
-      # provide prompt unless force flag is set
-      unless options[:force]
-        puts "Do you want to continue with rename operation?"
-        puts "(anything by Y aborts operation):"
-        $stdout.flush
-        $stderr.flush
-        answer = $stdin.gets.chomp
-
-        unless (answer == 'y' || answer == 'Y')
-          puts "OPERATION ABORTED"
-          return true
-        end
-      end
-
-      renames.keys.sort.each do |k|
-        FileUtils.mv(File.join(dir,renames[k].fname), File.join(dir, renames[k].newname),
-                     :verbose => options[:verbose], :noop => options[:noop])
-      end
-
-      puts "\nRENAME OPERATION COMPLETE"
-      puts "thank you for using #$0 "
-      puts "goodbye..."
-      puts "-------------------------------------------------------------------------------"
-      $stdout.flush
-      $stderr.flush
-      return true
-    end
-
-  end
-end
-=end
