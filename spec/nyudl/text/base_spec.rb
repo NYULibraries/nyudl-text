c0642d0c126217b14043d7d3004508110785e882
Joseph Pawletko <jgpawletko@gmail.com>
2013-10-07 13:44:10 -0400

    #rename_plan working

--- spec/nyudl/text/base_spec.rb
+++ spec/nyudl/text/base_spec.rb
@@ -265,21 +265,5 @@ describe Nyudl::Text::Base do
       end
     end
   end
-
-  context "when some files need renaming" do
-    it "#rename! only files requiring rename"
-    it "#renames returns empty hash when there are no renames"
-    it "#rename? returns true  when    renames required"
-    it "#rename? returns false when no renames required"
-    it "#errors  returns errors keyed by analysis type: unrecognized filenames, slot errors, sequence errors. "
-    it "#analyze  returns true  if there are no  errors"
-    it "#analyze  returns false if there are any errors"
-    it "#analyze!  raises an exception if there are any errors"
-    it "#analyzed? returns true if analysis was run"
-    it "#analyzed? returns false after a rename operation" # is this correct/expected?
-    it "#valid? returns" 
-    it ".initialize raises an exception if the argument is not a directory"
-    it ".initialize raises" 
-  end
 end
 
