ece725b61a5fb2a9b7a255c87fffb07e98ee5ec2
jgpawletko <jgpawletko@gmail.com>
2018-01-17 11:09:53 -0500

    encapsulate file ROLES

--- lib/nyudl/text/filename.rb
+++ lib/nyudl/text/filename.rb
@@ -6,7 +6,7 @@ module Nyudl
     # TODO: may want to introduce "type" and "role", where type is eoc, insert, oversized, target, ...
     # TODO: role is dmaker, master, dmaker_enhanced, dmaker_redacted
     class Filename
-
+      ROLES_SUBREGEX = 'o|de|d|m'
       # difference two filenames and return relationship
       # cases:
       # :unknown   - cannot determine relationship
@@ -20,8 +20,8 @@ module Nyudl
       # NOTE: TAKES STRINGS FOR ARGUMENTS
       def self.relationship(f1, f2)
         # nre = no role or extension
-        f1_nre = f1.sub(/(m|d|de)\.(?:tif|dng)/,'')
-        f2_nre = f2.sub(/(m|d|de)\.(?:tif|dng)/,'')
+        f1_nre = f1.sub(/(#{ROLES_SUBREGEX})\.(?:tif|dng)/,'')
+        f2_nre = f2.sub(/(#{ROLES_SUBREGEX})\.(?:tif|dng)/,'')
         if f1 == f2
           :identical
         elsif f1_nre == f2_nre
@@ -85,7 +85,7 @@ module Nyudl
           @role = 'eoc'
 
           # STANDARD PAGES
-        when /\A(#{@prefix})_(#{@pg_num.acc_rf})_?((o|de|d|m)\.(?:tif|dng))\z/   then
+        when /\A(#{@prefix})_(#{@pg_num.acc_rf})_?((#{ROLES_SUBREGEX})\.(?:tif|dng))\z/   then
           # numbered page, dmaker, old-style role
           # mss092_ref27_000001d.tif -> mss092_ref27_n000001_d.tif
           # mss092_ref27_001d.tif    -> mss092_ref27_n000001_d.tif
@@ -94,7 +94,7 @@ module Nyudl
           @newname = "#{@new_prefix}_#{@pg_num.fmt(pn)}_#{suffix}"
           @role    = determine_role($4)
 
-        when /\A(#{@prefix})(_|-)(#{@fr_num.acc_rf})_?((o|de|d|m)\.(?:tif|dng))\z/ then
+        when /\A(#{@prefix})(_|-)(#{@fr_num.acc_rf})_?((#{ROLES_SUBREGEX})\.(?:tif|dng))\z/ then
           # front matter, dmaker, old-style role
           # mss092_ref27-fr02d.tif -> mss092_ref27_afr02_d.tif
           fr     = $3.dup
@@ -102,7 +102,7 @@ module Nyudl
           @newname = "#{@new_prefix}_#{@fr_num.fmt(fr)}_#{suffix}"
           @role    = determine_role($5)
 
-        when /\A(#{@prefix})_(#{@bk_num.acc_rf})_?((o|de|d|m)\.(?:tif|dng))\z/ then
+        when /\A(#{@prefix})_(#{@bk_num.acc_rf})_?((#{ROLES_SUBREGEX})\.(?:tif|dng))\z/ then
           # back matter, dmaker, old-style role
           # mss092_ref27_bk02d.tif -> mss092_ref27_zbk02_d.tif
           bk     = $2.dup
@@ -116,7 +116,7 @@ module Nyudl
 
 
           # INSERTS (accepts and corrects _N or _NN)
-        when /\A(#{@prefix})_(#{@pg_num.acc_rf})_(#{@in_num.acc_rf})_?((o|de|d|m)\.(?:tif|dng))\z/   then
+        when /\A(#{@prefix})_(#{@pg_num.acc_rf})_(#{@in_num.acc_rf})_?((#{ROLES_SUBREGEX})\.(?:tif|dng))\z/   then
           # numbered page, old-style role
           pn     = $2.dup
           inum   = $3.dup
@@ -124,7 +124,7 @@ module Nyudl
           @newname = "#{@new_prefix}_#{@pg_num.fmt(pn)}_#{@in_num.fmt(inum)}_#{suffix}"
           @role    = determine_role($5)
 
-        when /\A(#{@prefix})(-|_)(#{@fr_num.acc_rf})_(#{@in_num.acc_rf})_?((o|de|d|m)\.(?:tif|dng))\z/ then
+        when /\A(#{@prefix})(-|_)(#{@fr_num.acc_rf})_(#{@in_num.acc_rf})_?((#{ROLES_SUBREGEX})\.(?:tif|dng))\z/ then
           # front matter, old-style role
           fr      = $3.dup
           inum    = $4.dup
@@ -132,7 +132,7 @@ module Nyudl
           @newname = "#{@new_prefix}_#{@fr_num.fmt(fr)}_#{@in_num.fmt(inum)}_#{suffix}"
           @role    = determine_role($6)
 
-        when /\A(#{@prefix})_(#{@bk_num.acc_rf})_(#{@in_num.acc_rf})_?((o|de|d|m)\.(?:tif|dng))\z/ then
+        when /\A(#{@prefix})_(#{@bk_num.acc_rf})_(#{@in_num.acc_rf})_?((#{ROLES_SUBREGEX})\.(?:tif|dng))\z/ then
           # back matter, old-style role
           bk     = $2.dup
           inum   = $3.dup
@@ -142,7 +142,7 @@ module Nyudl
 
 
           # OVERSIZED (accepts and corrects _N or _NN)
-        when /\A(#{@prefix})_(#{@pg_num.acc_rf})_(\d+)_(\d+)_?((o|de|d|m)\.(?:tif|dng))\z/   then
+        when /\A(#{@prefix})_(#{@pg_num.acc_rf})_(\d+)_(\d+)_?((#{ROLES_SUBREGEX})\.(?:tif|dng))\z/   then
           # numbered page, old-style role
           pn       = $2.dup
           olevel_1 = $3.dup
@@ -152,7 +152,7 @@ module Nyudl
           @newname = "#{@new_prefix}_#{@pg_num.fmt(pn)}_z#{'%02d' % olevel_1.gsub(/\A0+/, '')}_z#{'%02d' % olevel_2.gsub(/\A0+/, '')}_#{suffix}"
 
 
-        when /\A(#{@prefix})(-|_)(#{@fr_num.acc_rf})_(\d+)_(\d+)_?((o|de|d|m)\.(?:tif|dng))\z/ then
+        when /\A(#{@prefix})(-|_)(#{@fr_num.acc_rf})_(\d+)_(\d+)_?((#{ROLES_SUBREGEX})\.(?:tif|dng))\z/ then
           # front matter, old-style role
           fr       = $3.dup
           olevel_1 = $4.dup
@@ -161,7 +161,7 @@ module Nyudl
           @role    = determine_role($7)
           @newname = "#{@new_prefix}_#{@fr_num.fmt(fr)}_z#{'%02d' % olevel_1.gsub(/\A0+/, '')}_z#{'%02d' % olevel_2.gsub(/\A0+/, '')}_#{suffix}"
 
-        when /\A(#{@prefix})_(#{@bk_num.acc_rf})_(\d+)_(\d+)_?((o|de|d|m)\.(?:tif|dng))\z/ then
+        when /\A(#{@prefix})_(#{@bk_num.acc_rf})_(\d+)_(\d+)_?((#{ROLES_SUBREGEX})\.(?:tif|dng))\z/ then
           # back matter, old-style role
           bk       = $2.dup
           olevel_1 = $3.dup
@@ -174,17 +174,17 @@ module Nyudl
           # Replace prefix if new_prefix provided
           # using #out_rf because this section is for files that do not need correction
           # other than possible prefix replacement
-        when /\A(#{@prefix})(_#{@pg_num.out_rf}_(o|de|d|m)\.(?:tif|dng))/ ,
-          /\A(#{@prefix})(_#{@fr_num.out_rf}_(o|de|d|m)\.(?:tif|dng))/ ,
-          /\A(#{@prefix})(_#{@bk_num.out_rf}_(o|de|d|m)\.(?:tif|dng))/ ,
+        when /\A(#{@prefix})(_#{@pg_num.out_rf}_(#{ROLES_SUBREGEX})\.(?:tif|dng))/ ,
+          /\A(#{@prefix})(_#{@fr_num.out_rf}_(#{ROLES_SUBREGEX})\.(?:tif|dng))/ ,
+          /\A(#{@prefix})(_#{@bk_num.out_rf}_(#{ROLES_SUBREGEX})\.(?:tif|dng))/ ,
 
-          /\A(#{@prefix})(_#{@pg_num.out_rf}_#{@in_num.out_rf}_(o|de|d|m)\.(?:tif|dng))/ ,
-          /\A(#{@prefix})(_#{@fr_num.out_rf}_#{@in_num.out_rf}_(o|de|d|m)\.(?:tif|dng))/ ,
-          /\A(#{@prefix})(_#{@bk_num.out_rf}_#{@in_num.out_rf}_(o|de|d|m)\.(?:tif|dng))/ ,
+          /\A(#{@prefix})(_#{@pg_num.out_rf}_#{@in_num.out_rf}_(#{ROLES_SUBREGEX})\.(?:tif|dng))/ ,
+          /\A(#{@prefix})(_#{@fr_num.out_rf}_#{@in_num.out_rf}_(#{ROLES_SUBREGEX})\.(?:tif|dng))/ ,
+          /\A(#{@prefix})(_#{@bk_num.out_rf}_#{@in_num.out_rf}_(#{ROLES_SUBREGEX})\.(?:tif|dng))/ ,
 
-          /\A(#{@prefix})(_#{@pg_num.out_rf}_z\d{2}_z\d{2}_(o|de|d|m)\.(?:tif|dng))/ ,
-          /\A(#{@prefix})(_#{@fr_num.out_rf}_z\d{2}_z\d{2}_(o|de|d|m)\.(?:tif|dng))/ ,
-          /\A(#{@prefix})(_#{@bk_num.out_rf}_z\d{2}_z\d{2}_(o|de|d|m)\.(?:tif|dng))/ ,
+          /\A(#{@prefix})(_#{@pg_num.out_rf}_z\d{2}_z\d{2}_(#{ROLES_SUBREGEX})\.(?:tif|dng))/ ,
+          /\A(#{@prefix})(_#{@fr_num.out_rf}_z\d{2}_z\d{2}_(#{ROLES_SUBREGEX})\.(?:tif|dng))/ ,
+          /\A(#{@prefix})(_#{@bk_num.out_rf}_z\d{2}_z\d{2}_(#{ROLES_SUBREGEX})\.(?:tif|dng))/ ,
           /\A(#{@prefix})(_ztarget_m.tif)/
         then
           @newname = "#{@new_prefix}#{$2}"
